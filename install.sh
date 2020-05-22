8bc180173eabff296ab32bbd88bb6acf47e32508
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2020-05-22 11:53:36 -0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!usr/bin/env bash
+
+dir=~/.dotfiles
+olddir=~/.dotfiles/old
+
+doties=".vim .bashrc .gitconfig .globalgitignore .tmux.conf .vimrc .zshrc"
+
+start() {
+	doBackup()
+	doSymlink()
+}
+
+doBackup() {
+	echo "Backuping files to $olddir"
+	mkdir -p $olddir
+
+	for dot in $doties; do
+		echo "Moving $dot"
+		mv ~/$dotie $olddir
+	done
+
+	echo "Backup completed!"
+}
+
+doSymlink() {
+	echo "Creating symlinks from $dir to ~/"
+	mv $dir
+	
+	for dot in $doties; do
+		echo "Symlink $dot"
+		ln -s $dir/$dot ~/$dot
+	done
+
+	echo "Symlink completed!"
+}
+
+start
